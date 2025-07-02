c8cd8324368cfeb324dcc68238003b84a46c5e46
Fitrah0311 <dzakyzain0211@gmail.com>
2025-07-02 15:37:19 +0700

    Update Layanan.php
    menambahkan illuminate factory

--- app/Models/Layanan.php
+++ app/Models/Layanan.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 class Layanan extends Model
 {
+    use HasFactory;
     protected $fillable = ['nama_layanan', 'harga'];
 
 
