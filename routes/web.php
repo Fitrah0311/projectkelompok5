7a5b90938f8ace4ce9acd295e7a57b1252662df5
Fitrah0311 <dzakyzain0211@gmail.com>
2025-07-03 12:22:18 +0700

    Update web.php
    menghapus route::get reservasi, layanan dan pelanggan

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\LayananController;
-use App\Http\Controllers\Reservasi;
+use App\Http\Controllers\ReservasiController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/', [PelangganController::class, 'index']);
-Route::get('/', [LayananController::class, 'index']);
-Route::get('/', [ReservasiController::class, 'index']);
 
+Route::resource('reservasi', ReservasiController::class);
 Route::resource('pelanggan', PelangganController::class);
 Route::resource('layanan', LayananController::class);
-Route::resource('reservasi', LayananController::class);
